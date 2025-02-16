27bf5179cfde0d54f015205059ea6237cebc3bad
v <vbogos3@gmail.com>
2025-02-16 23:12:35 +0100

    Fix overlay dismiss on mobile browser screens

--- lib/widgets/artwork_grid.dart
+++ lib/widgets/artwork_grid.dart
@@ -1,6 +1,5 @@
 import 'package:exsy/models/artwork.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ArtworkGrid extends StatelessWidget {
   final List<Artwork> artworks;
@@ -27,8 +26,8 @@ class ArtworkGrid extends StatelessWidget {
           return GridView.builder(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: crossAxisCount,
-              crossAxisSpacing: 24.0,
-              mainAxisSpacing: 24.0,
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
               childAspectRatio: 1,
             ),
             itemCount: artworks.length,
@@ -50,11 +49,11 @@ class ArtworkGrid extends StatelessWidget {
                       child: Image.asset(artwork.imageUrl, fit: BoxFit.cover),
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 2.0),
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
                       child: Text(artwork.title, style: const TextStyle(fontSize: 16)),
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 2.0),
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
                       child: Text(artwork.description, maxLines: 2, overflow: TextOverflow.ellipsis),
                     ),
                   ],
@@ -105,105 +104,92 @@ class _ImageOverlayState extends State<ImageOverlay> {
       onTap: () {
         Navigator.of(context).pop();
       },
-      child: Focus(
-        autofocus: true,
-        onKey: (node, event) {
-          if (event is RawKeyDownEvent) {
-            if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-              _showPreviousImage();
-              return KeyEventResult.handled;
-            } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-              _showNextImage();
-              return KeyEventResult.handled;
-            }
-          }
-          return KeyEventResult.ignored;
-        },
-        child: Dialog(
-          backgroundColor: Colors.black.withOpacity(0.8),
-          insetPadding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 24),
-                        Expanded(
-                          child: Image.asset(widget.artworks[currentIndex].imageUrl, fit: BoxFit.contain),
+      child: Dialog(
+        backgroundColor: Colors.black.withOpacity(0.8),
+        insetPadding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 24),
+                      Expanded(
+                        child: Image.asset(widget.artworks[currentIndex].imageUrl, fit: BoxFit.contain),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(widget.artworks[currentIndex].title,
+                                style: const TextStyle(fontSize: 24, color: Colors.white)),
+                            const SizedBox(height: 4),
+                            Text(widget.artworks[currentIndex].description,
+                                style: const TextStyle(color: Colors.white)),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Text(widget.artworks[currentIndex].title,
-                                  style: const TextStyle(fontSize: 24, color: Colors.white)),
-                              const SizedBox(height: 4),
-                              Text(widget.artworks[currentIndex].description,
-                                  style: const TextStyle(color: Colors.white)),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
                   ),
                 ),
-                Positioned(
-                  left: 16,
-                  top: 0,
-                  bottom: 0,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(Icons.arrow_back, size: 40, color: Colors.white),
-                          onPressed: _showPreviousImage,
-                          splashRadius: 30,
-                        ),
+              ),
+              Positioned(
+                left: 16,
+                top: 0,
+                bottom: 0,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back, size: 40, color: Colors.white),
+                        onPressed: _showPreviousImage,
+                        splashRadius: 30,
                       ),
                     ),
                   ),
                 ),
-                Positioned(
-                  right: 16,
-                  top: 0,
-                  bottom: 0,
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(Icons.arrow_forward, size: 40, color: Colors.white),
-                          onPressed: _showNextImage,
-                          splashRadius: 30,
-                        ),
+              ),
+              Positioned(
+                right: 16,
+                top: 0,
+                bottom: 0,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_forward, size: 40, color: Colors.white),
+                        onPressed: _showNextImage,
+                        splashRadius: 30,
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
