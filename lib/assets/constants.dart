6862ede4875097db83a4f958d48ea8ec6f6e6b08
v <vbogos3@gmail.com>
2025-05-06 00:39:03 +0200

    Update contact info

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -4,11 +4,11 @@ class Constants {
   static const labelAlbum = 'Album';
   static const labelAllArtworks = 'All Artworks';
   static const labelExploreArtworks = 'Explore Artworks →';
-  static const labelInstagram = '@buntovac_';
+  static const labelInstagram = '@jelena_milatovic';
   static const labelNA = 'N/A';
   static const labelNameSurname = 'Jelena Milatović';
   static const labelSelectAlbum = 'Select Album';
 
-  static const urlInstagramNative = 'instagram://user?username=buntovac_';
-  static const urlInstagramWeb = 'https://www.instagram.com/buntovac_/';
+  static const urlInstagramNative = 'instagram://user?username=jelena_milatovic';
+  static const urlInstagramWeb = 'https://www.instagram.com/jelena_milatovic/';
 }
